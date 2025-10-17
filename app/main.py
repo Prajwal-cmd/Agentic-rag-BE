a8855b9dbbe1d2ebbbadaab953997072484eaf58
Prajwal-cmd <prajwalkanojia95@gmail.com>
2025-10-17 18:48:06 +0530

    main.py 1

--- app/main.py
+++ app/main.py
@@ -990,6 +990,6 @@ async def extract_math(
 
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
